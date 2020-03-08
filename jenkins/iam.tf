c4b580c36361d6f068640fd0098e157ff7f5a02d
Alfredo Pedraza <alfpedraza@hotmail.com>
2020-03-08 07:28:02 +0000

    Add comments to describe the iam resources

--- jenkins/iam.tf
+++ jenkins/iam.tf
@@ -1,7 +1,15 @@
+# ----------------------------------------------------------------------------#
+# Create instance profile that the Jenkins server will hold                   #
+# ----------------------------------------------------------------------------#
+
 resource "aws_iam_instance_profile" "jenkins" {
   name = "${var.project_name}-${var.region_name}-jenkins"
   role = aws_iam_role.jenkins.name
 }
+
+# ----------------------------------------------------------------------------#
+# Create the IAM role for the Jenkins server                                  #
+# ----------------------------------------------------------------------------#
 
 resource "aws_iam_role" "jenkins" {
   name               = "${var.project_name}-${var.region_name}-jenkins"
