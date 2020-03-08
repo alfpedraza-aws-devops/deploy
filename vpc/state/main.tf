4121047f44911c2fb801e51e974755e6393ffb1f
Alfredo Pedraza <alfpedraza@hotmail.com>
2020-03-08 22:01:58 +0000

    Change the name of the state bucket

--- vpc/state/main.tf
+++ vpc/state/main.tf
@@ -10,6 +10,6 @@ provider "aws" {
 module "terraform_state" {
   source = "../../modules/terraform-state"
   region_name = var.region_name
-  bucket_name = join("-", [var.account_id, var.project_name, "bucket", var.terraform_module])    
-  table_name  = join("-", [var.account_id, var.project_name, "table" , var.terraform_module])
+  bucket_name = join("-", [var.account_id, var.project_name, var.terraform_module, "terraform-state"])    
+  table_name  = join("-", [var.account_id, var.project_name, var.terraform_module, "terraform-lock" ])
 }
